a9b080c976d5192a1a13e244c20c969f8b760f57
andcia <andreciappara@gmail.com>
2017-05-23 00:16:30 +0200

    Added New scripts
    Player objects includes a rigidbody to it and also a box collider which
    will not be in use for now.
    
    Player Controller Script that will control the player throughout
    
    Player movement in player controller script allows the player to move
    either with the W,A,S,D or else with the arrow keys.
    
    Camera Follow Script that will follow the player according to the
    keys/direction set by the user

---
+++ Game Design Project/Assets/Scripts/cameraFollowScript.cs
@@ -0,0 +1,32 @@
+﻿// John Scerri 12/5/17
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowScript : MonoBehaviour
+{
+
+	public GameObject player;
+	Vector3 payerPos = new Vector3();
+	Vector3 camPos = new Vector3();
+	float dist;
+
+	// Use this for initialization
+	void Start()
+	{
+		camPos = gameObject.transform.position;
+		payerPos = player.transform.position;
+		dist = payerPos[2] - gameObject.transform.position.z;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		payerPos = player.transform.position;
+
+		gameObject.transform.position = new Vector3(payerPos[0], camPos[1], payerPos[2] - dist);
+
+		//Change  background colour Day/night (10 mins)
+		Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, Color.black, Time.deltaTime / 60);
+	}
+}
